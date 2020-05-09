b989faaac171615552f505f99a37aa939ce5223b
Rainspistols <ffly@mail.ru>
2020-05-09 12:21:44 +0200

    Updates

---
+++ precache-manifest.cbeca982678795451a4b17ecbc943aef.js
@@ -0,0 +1,310 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae57acdfb08d69d9553644df9144276",
+    "url": "/Stop-climate-change/index.html"
+  },
+  {
+    "revision": "9bf0556e32af954bf170",
+    "url": "/Stop-climate-change/static/css/2.55dfd4ec.chunk.css"
+  },
+  {
+    "revision": "ebe62968757b3619ea6d",
+    "url": "/Stop-climate-change/static/css/main.632126f8.chunk.css"
+  },
+  {
+    "revision": "9bf0556e32af954bf170",
+    "url": "/Stop-climate-change/static/js/2.3c72753c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Stop-climate-change/static/js/2.3c72753c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe62968757b3619ea6d",
+    "url": "/Stop-climate-change/static/js/main.dc57c5e4.chunk.js"
+  },
+  {
+    "revision": "50d412b14d78c50b17bd",
+    "url": "/Stop-climate-change/static/js/runtime-main.da93f538.js"
+  },
+  {
+    "revision": "a9bd2d61a0f64fd093d3a79b311c4810",
+    "url": "/Stop-climate-change/static/media/1--tablet.a9bd2d61.jpg"
+  },
+  {
+    "revision": "602a35389767766eb3d9c24b691162c1",
+    "url": "/Stop-climate-change/static/media/1.602a3538.jpg"
+  },
+  {
+    "revision": "7b7434a9d174d8a68d39c68374cbe2d5",
+    "url": "/Stop-climate-change/static/media/1.7b7434a9.jpg"
+  },
+  {
+    "revision": "7e0fc42cf6b87920edfb2c9eb8824512",
+    "url": "/Stop-climate-change/static/media/2--tablet.7e0fc42c.jpg"
+  },
+  {
+    "revision": "1aa5110a668fea1c85f655b5258f2ecf",
+    "url": "/Stop-climate-change/static/media/2.1aa5110a.jpg"
+  },
+  {
+    "revision": "fce9f8b90f1338d02d9e7fc96e2b1ede",
+    "url": "/Stop-climate-change/static/media/2.fce9f8b9.jpg"
+  },
+  {
+    "revision": "3b12b0032ff88d0693d61da7be624cdb",
+    "url": "/Stop-climate-change/static/media/3--tablet.3b12b003.jpg"
+  },
+  {
+    "revision": "0aa0786e9b758949df38423dae977fcd",
+    "url": "/Stop-climate-change/static/media/3.0aa0786e.jpg"
+  },
+  {
+    "revision": "68e8abaec9ead77985071cb3ce7a352c",
+    "url": "/Stop-climate-change/static/media/3.68e8abae.jpg"
+  },
+  {
+    "revision": "92eb44826f2ec2db4ca100663dfd182e",
+    "url": "/Stop-climate-change/static/media/4.92eb4482.jpg"
+  },
+  {
+    "revision": "a948be0e6545f27e6e75c1f5903e6493",
+    "url": "/Stop-climate-change/static/media/5.a948be0e.jpg"
+  },
+  {
+    "revision": "e1f4d19f998c145be85737c007938d4c",
+    "url": "/Stop-climate-change/static/media/6.e1f4d19f.jpg"
+  },
+  {
+    "revision": "a9069ee48f19bbf1b7d9df27b8dc9c01",
+    "url": "/Stop-climate-change/static/media/Group 51.a9069ee4.svg"
+  },
+  {
+    "revision": "2ee379cd9d50b00b068e035dbccab0ab",
+    "url": "/Stop-climate-change/static/media/Icon Books.2ee379cd.svg"
+  },
+  {
+    "revision": "76cbd7cb9b19bcbd73e81bb9e22ca7c0",
+    "url": "/Stop-climate-change/static/media/Icon Earth.76cbd7cb.svg"
+  },
+  {
+    "revision": "dbd9e6fcee4346970a9dca9b9ca7a636",
+    "url": "/Stop-climate-change/static/media/Icon Flower.dbd9e6fc.svg"
+  },
+  {
+    "revision": "c932b886f14f0ad86c87cd7d4bd2b1d9",
+    "url": "/Stop-climate-change/static/media/Icon Hand.c932b886.svg"
+  },
+  {
+    "revision": "01dab2e5a249b3225f638f66ac9851b3",
+    "url": "/Stop-climate-change/static/media/Icon Heart.01dab2e5.svg"
+  },
+  {
+    "revision": "be7d50752381095b33430a782f0b99de",
+    "url": "/Stop-climate-change/static/media/Icon People.be7d5075.svg"
+  },
+  {
+    "revision": "f60b187b665f0627107458017e790a0e",
+    "url": "/Stop-climate-change/static/media/Icon Person.f60b187b.svg"
+  },
+  {
+    "revision": "eb3f1d57eb750cc71dad48cdcb0a0929",
+    "url": "/Stop-climate-change/static/media/Icon Sun.eb3f1d57.svg"
+  },
+  {
+    "revision": "18e084ab912242f8bf964ad34510036b",
+    "url": "/Stop-climate-change/static/media/Path 85.18e084ab.svg"
+  },
+  {
+    "revision": "c4eefd5d22104aa2dd4cf7b1bf658a10",
+    "url": "/Stop-climate-change/static/media/Path 87.c4eefd5d.svg"
+  },
+  {
+    "revision": "c71d60264aa50177202f83c9a00f0b7c",
+    "url": "/Stop-climate-change/static/media/arrowRight.c71d6026.svg"
+  },
+  {
+    "revision": "4246b21f13ce997c259ecc591da785a8",
+    "url": "/Stop-climate-change/static/media/bg--Tablet.4246b21f.jpg"
+  },
+  {
+    "revision": "0c2e7481e1349d126fbac0bcfa860160",
+    "url": "/Stop-climate-change/static/media/bg--desktop.0c2e7481.jpg"
+  },
+  {
+    "revision": "1c484742f225da1649b91e313e91c211",
+    "url": "/Stop-climate-change/static/media/bg--desktop.1c484742.jpg"
+  },
+  {
+    "revision": "60a544ed90a3315e43f539396bb37434",
+    "url": "/Stop-climate-change/static/media/bg--desktop.60a544ed.jpg"
+  },
+  {
+    "revision": "8cd4bee8b2aeeaa77d937dfad07f37d8",
+    "url": "/Stop-climate-change/static/media/bg--desktop.8cd4bee8.jpg"
+  },
+  {
+    "revision": "b65688903ec71a2114848556c95f1a06",
+    "url": "/Stop-climate-change/static/media/bg--desktop.b6568890.jpg"
+  },
+  {
+    "revision": "dca4d1f142179fc95ca5168791aa6ef8",
+    "url": "/Stop-climate-change/static/media/bg--desktop.dca4d1f1.jpg"
+  },
+  {
+    "revision": "eee5cf0bf0a270cffecbd643b0d9ab71",
+    "url": "/Stop-climate-change/static/media/bg--desktop.eee5cf0b.jpg"
+  },
+  {
+    "revision": "3cfb1980f0276822f37b421deb0b22ef",
+    "url": "/Stop-climate-change/static/media/bg--mobile.3cfb1980.jpg"
+  },
+  {
+    "revision": "2c104377ef6dbb6a6db90e2fcceac13e",
+    "url": "/Stop-climate-change/static/media/bg--tablet.2c104377.jpg"
+  },
+  {
+    "revision": "b36a107c0fae83c025e75dae08c98ef2",
+    "url": "/Stop-climate-change/static/media/bg--tablet.b36a107c.jpg"
+  },
+  {
+    "revision": "beb977de1491fb743252f532e41170d1",
+    "url": "/Stop-climate-change/static/media/bg--tablet.beb977de.jpg"
+  },
+  {
+    "revision": "3233a7e8fcd244ff676da14ae50f704d",
+    "url": "/Stop-climate-change/static/media/bg.3233a7e8.jpg"
+  },
+  {
+    "revision": "5f130ac4b1aa9d996e56eb6efd29b86f",
+    "url": "/Stop-climate-change/static/media/bg.5f130ac4.jpg"
+  },
+  {
+    "revision": "65396a2e97eb82943336cf6c263968bf",
+    "url": "/Stop-climate-change/static/media/bg.65396a2e.jpg"
+  },
+  {
+    "revision": "6882780591b1721e8c64dc3217163078",
+    "url": "/Stop-climate-change/static/media/bg.68827805.jpg"
+  },
+  {
+    "revision": "90e2453feb0f21fdb5d7c924c1bde42c",
+    "url": "/Stop-climate-change/static/media/bg.90e2453f.jpg"
+  },
+  {
+    "revision": "c40a84077685aee3c48cc0021b3db1d6",
+    "url": "/Stop-climate-change/static/media/bg.c40a8407.jpg"
+  },
+  {
+    "revision": "d3d7745ef44cf37514f454d76f281413",
+    "url": "/Stop-climate-change/static/media/bg.d3d7745e.jpg"
+  },
+  {
+    "revision": "39db9f862b1f9b88f1a07d92959cab67",
+    "url": "/Stop-climate-change/static/media/facebook.39db9f86.svg"
+  },
+  {
+    "revision": "39f9dc8c531ffbc1d9d8c80b7413a17e",
+    "url": "/Stop-climate-change/static/media/iconmonstr-facebook-1.39f9dc8c.svg"
+  },
+  {
+    "revision": "7186f82ad777bee64dd42b01b917a3c4",
+    "url": "/Stop-climate-change/static/media/iconmonstr-facebook-1.7186f82a.svg"
+  },
+  {
+    "revision": "e9658eae791afef3520f34cd4d82be6d",
+    "url": "/Stop-climate-change/static/media/iconmonstr-twitter-1.e9658eae.svg"
+  },
+  {
+    "revision": "1b2c2a5b6795a09083ced8c64e6a5720",
+    "url": "/Stop-climate-change/static/media/img--desktop.1b2c2a5b.jpg"
+  },
+  {
+    "revision": "64c563825c167148c59588f2fda72274",
+    "url": "/Stop-climate-change/static/media/img.64c56382.jpg"
+  },
+  {
+    "revision": "9877240b25d9e8d729c0e48f5fa2dd11",
+    "url": "/Stop-climate-change/static/media/img1--desktop.9877240b.jpg"
+  },
+  {
+    "revision": "4597339bef142e9f7607f060c01c06c6",
+    "url": "/Stop-climate-change/static/media/img1--tablet.4597339b.jpg"
+  },
+  {
+    "revision": "be50f599a9b8d1e92e17cc85a0d23755",
+    "url": "/Stop-climate-change/static/media/img1.be50f599.jpg"
+  },
+  {
+    "revision": "c4184bbdaeaf586cb4f65a3ff979377b",
+    "url": "/Stop-climate-change/static/media/meeting.c4184bbd.jpg"
+  },
+  {
+    "revision": "6f63d6a928352cfb56ca443eb615ae9b",
+    "url": "/Stop-climate-change/static/media/people.6f63d6a9.jpg"
+  },
+  {
+    "revision": "2139a23b8aee5f89b1cbd4642f8badc3",
+    "url": "/Stop-climate-change/static/media/post1.2139a23b.jpg"
+  },
+  {
+    "revision": "4b1b44bcefe172d87a8f814e7a84e25d",
+    "url": "/Stop-climate-change/static/media/post1tablet.4b1b44bc.jpg"
+  },
+  {
+    "revision": "63a801de0ea20d90ecb6c9598e01078a",
+    "url": "/Stop-climate-change/static/media/post2.63a801de.jpg"
+  },
+  {
+    "revision": "b0568f0b31f7ad79c41d8e7b917ab8f5",
+    "url": "/Stop-climate-change/static/media/post2tablet.b0568f0b.jpg"
+  },
+  {
+    "revision": "e49959e01bc689d19b2d19b0b00a250b",
+    "url": "/Stop-climate-change/static/media/postFull.e49959e0.jpg"
+  },
+  {
+    "revision": "c324ef345dfcb263cf77551a1c1d752c",
+    "url": "/Stop-climate-change/static/media/postHalf.c324ef34.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Stop-climate-change/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Stop-climate-change/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Stop-climate-change/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Stop-climate-change/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7bd198dd99de7b5498465ec3934fe60b",
+    "url": "/Stop-climate-change/static/media/social-1_logo-instagram.7bd198dd.svg"
+  },
+  {
+    "revision": "1f399f261c746e79b54228930839ceea",
+    "url": "/Stop-climate-change/static/media/social-1_logo-twitter.1f399f26.svg"
+  },
+  {
+    "revision": "e67754c16689c7b83699470bb3680d46",
+    "url": "/Stop-climate-change/static/media/social-1_logo-youtube.e67754c1.svg"
+  },
+  {
+    "revision": "dd611edb5d55124aef1401afff64ed10",
+    "url": "/Stop-climate-change/static/media/tree-planting.dd611edb.jpg"
+  },
+  {
+    "revision": "d8834122c87a15744d28b52bb8f0d297",
+    "url": "/Stop-climate-change/static/media/tree.d8834122.jpg"
+  },
+  {
+    "revision": "9f64b0ca59d926009078a91d7dc5d6ff",
+    "url": "/Stop-climate-change/static/media/twitter.9f64b0ca.svg"
+  }
+]);
